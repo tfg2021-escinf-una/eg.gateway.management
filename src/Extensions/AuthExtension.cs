ae84571d3c1f8c9499a5b2cb9626d814d314f4d7
Lobo, Edwin <edlobo98@gmail.com>
2022-03-21 22:04:08 -0600

    initializing ocelot configuration

---
+++ src/Extensions/AuthExtension.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Text;
+
+namespace EG.Gateway.Microservice.Extensions
+{
+    public static class AuthExtension
+    {
+        private static IConfiguration Configuration;
+
+        public static IServiceCollection AddJwtAuthentication(this IServiceCollection services)
+        {
+            
+            using(var sp = services.BuildServiceProvider())
+            {
+                Configuration = sp.GetRequiredService<IConfiguration>();
+            }
+
+            services.AddAuthentication()
+                .AddJwtBearer("SECURITY-TOKEN", options =>
+                {
+                    options.RequireHttpsMetadata = false;
+                    options.SaveToken = true;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = Configuration["Jwt:Issuer"],
+                        ValidAudience = Configuration["Jwt:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:SecretKey"])),
+                        RequireExpirationTime = true,
+                        ClockSkew = TimeSpan.Zero,
+                    };
+                });
+
+            return services;
+        }
+
+    }
+}
